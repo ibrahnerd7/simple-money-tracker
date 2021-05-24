ce5968b61cb6f3f2e1322022e100ef117e9e8260
ibrahnerd7 <ibrahmaina7@gmail.com>
2021-05-24 21:46:05 +0300

    Add category model

---
+++ src/models/category.model.ts
@@ -0,0 +1,38 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Category extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  name: string;
+
+  @property({
+    type: 'string',
+  })
+  avatar_url?: string;
+
+  @property({
+    type: 'date',
+  })
+  created_at?: string;
+
+
+  constructor(data?: Partial<Category>) {
+    super(data);
+  }
+}
+
+export interface CategoryRelations {
+  // describe navigational properties here
+}
+
+export type CategoryWithRelations = Category & CategoryRelations;
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,3 +1,4 @@
 export * from './user.model';
 export * from './wallet.model';
 export * from './transaction.model';
+export * from './category.model';
